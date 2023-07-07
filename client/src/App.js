cf47d94c5b14bb07b2e5d4c9a9160d52bf900375
Nicolas Panozo <nicopanozoc@gmail.com>
2023-07-07 00:59:07 -0400

    sql script v1

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import ListHeader from "./components/ListHeader"
 const App = () => {
   return (
     <div className="app">
-      <ListHeader listName={'Holiday Tick list'}></ListHeader>
+      <ListHeader listName={'🏝️ Holiday Tick list'}></ListHeader>
     </div>
   )
 }
